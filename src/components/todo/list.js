fa6bc13a7049aed7e85d7f69bc2c68511ef377a2
wafaankoush99 <wafadirawe@gmail.com>
2021-06-28 13:45:49 +0300

    mod lab 30

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -15,17 +15,17 @@ function TodoList(props) {
     const [flag, setFlag] = useState(false)
     const [id, setId] = useState('')
 
-    const toggleHandler = e => {
+    const toggle = id => {
         setFlag(!flag)
-        setId(e)
+        setId(id)
     }
 
-    const editHandler = e => {
+    const editTask = e => {
         e.preventDefault();
-        toggleHandler(e);
+        toggle(id);
 
         let update = e.target.text.value;
-        props.editHandler(update, e)
+        props.editTask(update, id)
     }
 
     return (
@@ -37,16 +37,28 @@ function TodoList(props) {
                         className={`complete-${item.complete.toString()}`}
                         key={item._id}
                     >
+
+
                         <span onClick={() => props.handleComplete(item._id)}>
                             {item.text} : {item.assignee} , difficulty :  {item.difficulty} , due : {item.duedate}
                         </span>
+
+                        <br></br>
+
+                        <div className="btns">
+
+                            <Button variant="info" onClick={() => toggle(item._id)} value={item._id}>Edit</Button>{' '}
+                            <Button variant="danger" onClick={() => props.deleteTask(item._id)} value={item._id}>Delete</Button>
+
+                        </div>
+
                     </ListGroup.Item>
                 ))}
             </ListGroup>
 
             <If condition={flag}
             >
-                <Form> onSubmit={editHandler}
+                <Form onSubmit={editTask}>
 
                     <Form.Label>
                         <span>Edit Task</span>
@@ -67,3 +79,78 @@ function TodoList(props) {
 
 
 export default TodoList;
+
+/////////////////////////////////////////////////
+
+
+// import React from 'react';
+// import { useState } from 'react';
+// import If from './If'
+// import { Button } from 'react-bootstrap';
+// import { ListGroup, Form } from 'react-bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// function TodoList(props) {
+
+//     const [flag, setFlag] = useState(false);
+//     const [id, setId] = useState('');
+
+
+
+
+//     const toggle = (id) => {
+//         setFlag(!flag);
+//         setId(id)
+//     }
+
+//     const editor = e => {
+//         e.preventDefault();
+//         toggle(id);
+//         let newUpdate = e.target.text.value
+//         props.editor(newUpdate, id)
+//     }
+
+//     return (
+
+//         <>
+//             <ListGroup>
+//                 {props.list.map(item => (
+
+//                     <ListGroup.Item action variant={item.complete ? 'dark' : 'light'}
+
+
+//                         className={`complete-${item.complete.toString()}`}
+//                         key={item._id}
+//                     >
+
+//                         <Button variant="outline-danger" onClick={() => props.deleteH(item._id)} value={item._id}>Delete</Button>
+//                         <Button variant="outline-secondary" onClick={() => toggle(item._id)} value={item._id}>Edit</Button>{' '}
+
+
+
+//                         <span onClick={() => props.handleComplete(item._id)}>
+//                             {item.text}  : {item.assignee} ,  difficulty : {item.difficulty} , due : {item.duedate}
+//                         </span>
+//                     </ListGroup.Item>
+//                 ))}
+//             </ListGroup>
+
+//             <If condition={flag}>
+//                 <Form onSubmit={editor}>
+
+//                     <Form.Label>
+//                         <span>Edit Task</span>
+//                         <Form.Control type="text" name="text" />
+//                     </Form.Label>
+//                     <Button variant="outline-secondary" type='submit' >Submit Edit</Button>
+//                 </Form>
+//             </If>
+//         </>
+//     );
+// }
+
+
+
+
+
+
+// export default TodoList;
--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -32,8 +32,8 @@ function ToDo(props) {
 
         if (item._id) {
             item.complete = !item.complete;
-            let list = list.map(listItem => listItem._id === item._id ? item : listItem);
-            setList({ list });
+            let listt = list.map(listItem => listItem._id === item._id ? item : listItem);
+            setList( listt );
         }
 
     };
@@ -41,7 +41,7 @@ function ToDo(props) {
     useEffect(() => {
 
 
-        let list = [
+        let listtt = [
             { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A', duedate: '2021-06-27' },
             { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A', duedate: '2021-06-27' },
             { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person B', duedate: '2021-06-27' },
@@ -49,22 +49,22 @@ function ToDo(props) {
             { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B', duedate: '2021-06-27' },
         ];
 
-        setList({ list });
+        setList( listtt );
 
     }, [])
 
-    const deleteTask = e => {
-        let list = list.filter((it) => it._id !== e) || {}
-        setList(list)
+    const deleteTask = id => {
+        let listttt = list.filter((it) => it._id !== id) || {}
+        setList(listttt)
     }
 
-    const editTask = (text, e) => {
-        let task = list.filter((it) => it._id === e)[0] || {}
+    const editTask = (text, id) => {
+        let task = list.filter((it) => it._id === id)[0] || {}
 
         if (task) {
             task.text = text;
-            let list = list.map(it => {
-                if (task._id === e) {
+            let listtttt = list.map(it => {
+                if (it._id === id) {
                     return task;
                 }
 
@@ -73,7 +73,7 @@ function ToDo(props) {
                 }
             })
 
-            setList(list)
+            setList(listtttt)
         }
     }
 
@@ -115,3 +115,104 @@ function ToDo(props) {
 // }
 
 export default ToDo;
+
+
+// /////////////////////////////////////////////
+
+// import React from 'react';
+// import TodoForm from './form.js';
+// import TodoList from './list.js';
+// import {useState} from 'react';
+// import {useEffect} from 'react';
+// import {Badge } from 'react-bootstrap'
+// import './todo.scss';
+
+
+// function ToDo(props) {
+  
+// const [list , setList] = useState([])
+
+
+//   const addItem = (item) => {
+//     item._id = Math.random();
+//     item.complete = false;
+//     setList( [...list, item]);
+//   };
+
+//   const toggleComplete = id => {
+
+//     let item = list.filter(i => i._id === id)[0] || {};
+
+//     if (item._id) {
+//       item.complete = !item.complete;
+//       let list2 = list.map(listItem => listItem._id === item._id ? item : listItem);
+//       setList(list2);
+//     }
+
+//   };
+
+//   useEffect(()=> {
+//     let list3 = [
+//       { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A' ,duedate : '2021-06-27'},
+//       { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A', duedate : '2021-06-27'},
+//       { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person B', duedate : '2021-06-27'},
+//       { _id: 4, complete: true, text: 'Do Homework', difficulty: 3, assignee: 'Person C' ,duedate : '2021-06-27'},
+//       { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B' , duedate : '2021-06-27'},
+//     ];
+
+//     setList(list3);
+//   },[])
+
+//   const deleteH =(id)=>{
+//     let list4 = list.filter((item)=>item._id !== id) || {}
+//     setList (list4)
+//   }
+
+//     const editTask = (text , id)=>{
+//       let item = list.filter ((item)=> item._id === id)[0] || {}
+//       if (item) {
+//         item.text = text;
+//         let list5 = list.map (itm =>{
+//           if (itm._id === id ){
+//             return item 
+//           }else {
+//             return itm
+//           }
+//         })
+//         setList (list5)
+//       }
+     
+     
+//       }
+    
+//       document.title = `Tasks left : ${list.filter((item) => !item.complete).length}`;
+
+//     return (
+//       <>
+//         <header>
+//           <h2>
+//           There are <Badge variant="secondary">{list.filter(item => !item.complete).length} </Badge> Items To Complete
+//           </h2>
+//         </header>
+
+//         <section className="todo">
+
+//           <div>
+//             <TodoForm handleSubmit={addItem} />
+//           </div>
+
+//           <div>
+//             <TodoList
+//               list={list}
+//               handleComplete={toggleComplete}
+//               deleteH = {deleteH}
+//               editTask = {editTask}
+//             />
+//           </div>
+//         </section>
+//       </>
+//     );
+  
+// }
+
+// export default ToDo;
